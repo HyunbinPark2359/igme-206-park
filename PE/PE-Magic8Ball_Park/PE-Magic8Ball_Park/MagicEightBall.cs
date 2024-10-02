c627d56c49aec50d90a43d5d05392da3d38ca68d
HyunbinPark2359 <hp2359@rit.edu>
2024-10-01 21:17:38 -0400

    Submitting PE - Magic 8 Ball

---
+++ PE/PE-Magic8Ball_Park/PE-Magic8Ball_Park/MagicEightBall.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PE_Magic8Ball_Park
+{
+    internal class MagicEightBall
+    {
+        // Include private fields
+        private string owner;       // Who's holding the magic 8 ball
+        private int timesShaken;    // The number of times the ball has been shaken
+        private string[] responses; // Colletion of responses
+        Random rng;                 // Random object
+
+
+        // Parameterized constructor
+
+        // Define all 4 fields
+        public MagicEightBall(string owner)
+        {
+            this.owner = owner;
+            this.timesShaken = 0;
+            this.responses = new[] {"It is certain.", "As I see it, yes.", 
+                             "Replay hazy, try again.", "Don't count on it.", "Very doubtful."};
+            rng = new Random();
+        }
+
+
+        // Methods
+
+        // Simulate shaking the magic 8 ball
+        public string ShakeBall()
+        {
+            // Increase the number of times shaken
+            timesShaken++;
+
+            // Randomly choose one of the responses and return it
+            return responses[rng.Next(responses.Length)];
+        }
+
+        // Return a statement about how many times the ball has been shaken
+        public string Report()
+        {
+            switch(timesShaken)
+            {
+                // Ball has not been shaken
+                case 0:
+                    return String.Format("{0} has not shaken the ball yet.", owner);
+                    break;
+
+                // Ball has been shaken 1 - 3 times
+                case 1:
+                case 2:
+                case 3:
+                    return String.Format("{0} has shaken the ball {1} times.", 
+                                         owner, timesShaken);
+                    break;
+
+                // Ball has been shaken more than 3 times
+                default:
+                    return String.Format("{0} has shaken the ball {1} times. " +
+                                         "That's a lot of questions!", owner, timesShaken);
+                    break;
+            }
+        }
+    }
+}
---
+++ PE/PE-Magic8Ball_Park/PE-Magic8Ball_Park/Program.cs
@@ -0,0 +1,72 @@
+﻿/*
+ * Hyunbin Park
+ * PE - Magic 8 Ball
+ * https://docs.google.com/document/d/13OBHKxRLnRKNiYijs119CSW9Y7kW77oRCd1uovYR-Hs/edit?usp=sharing
+ */
+
+namespace PE_Magic8Ball_Park
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Store user's input
+            string userData;
+
+
+            // Prompt for the name of the ball's owner
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.Write("Welcome to Magic 8 Ball Simulator!" + 
+                          "\n  > Who owns this ball?  ");
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            userData = Console.ReadLine();
+            Console.ForegroundColor = ConsoleColor.White;
+
+            // Instantiate a MagicEightBall object
+            MagicEightBall magicEightBall = new MagicEightBall(userData);
+
+
+            // Ask the user what they'd like to do with the ball
+            // until user manually quits
+            do
+            {
+                Console.Write("\nWhat would you like to do?" +
+                              "\nYou can 'shake' the ball, get a 'report', or 'quit':  ");
+                Console.ForegroundColor = ConsoleColor.Cyan;
+                userData = Console.ReadLine().ToLower().Trim();
+                Console.ForegroundColor = ConsoleColor.White;
+
+                // Perform the corresponding action
+                switch (userData)
+                {
+                    // Prompt for question and shake the ball
+                    // Print the response of the ball
+                    case "shake":
+                        Console.Write("  > What is your question? ");
+                        Console.ForegroundColor = ConsoleColor.Cyan;
+                        Console.ReadLine();
+                        Console.ForegroundColor = ConsoleColor.White;
+                        Console.WriteLine("  > The Magic 8 Ball says: {0}", 
+                                          magicEightBall.ShakeBall());
+                        break;
+
+                    // Print the report
+                    case "report":
+                        Console.WriteLine("  > {0}", magicEightBall.Report());
+                        break;
+
+                    // Say goodbye and exit the loop
+                    case "quit":
+                        Console.WriteLine("  > Goodbye!");
+                        break;
+
+                    // Anything else
+                    default:
+                        Console.WriteLine("  > I do not recognize that response.");
+                        break;
+                }
+            }
+            while (userData != "quit");
+        }
+    }
+}
