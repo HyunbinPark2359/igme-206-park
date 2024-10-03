c9c5c0f1e0caf732099509520714da534569dae0
HyunbinPark2359 <hp2359@rit.edu>
2024-10-03 19:17:56 -0400

    Submitting PE - Properties

---
+++ PE/PE-Properties_Park/PE-Properties_Park/Book.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PE_Properties_Park
+{
+    internal class Book
+    {
+        // 5 Private fields
+        private string title;       // The title of book
+        private string author;      // The author of book
+        private int numberOfPages;  // The number of pages in book
+        private string owner;       // The owner of book
+        private int totalTimesRead; // How many times the owner read the book
+
+
+        // Parameterized constructor
+        public Book(string title, string author, int numberOfPages, string owner)
+        {
+            this.title = title;
+            this.author = author;
+            this.numberOfPages = numberOfPages;
+            this.owner = owner;
+            totalTimesRead = 0;
+        }
+
+
+        // 3 get-only properties and 2 get & set properties
+
+        // Get the title of book
+        public string Title
+        {
+            get { return title; }
+        }
+
+        // Get the author of book
+        public string Author
+        {
+            get { return author; }
+        }
+
+        // Get the number of pages
+        public int NumberOfPages
+        {
+            get { return numberOfPages; }
+        }
+
+        // Get and set the owner of the book
+        public string Owner
+        {
+            get { return owner; }
+            set
+            {
+                if (value != null && value.Trim().Length > 0)
+                {
+                    owner = value;
+                }
+            }
+        }
+
+        // Get and set the number of times read the book
+        public int TotalTimesRead
+        {
+            get { return totalTimesRead; }
+            set
+            {
+                if (totalTimesRead < value)
+                {
+                    totalTimesRead = value;
+                }
+            }
+        }
+
+
+        // Method
+
+        // Print all of the book's information
+        public void Print()
+        {
+            Console.WriteLine("{0} by {1} has {2} pages. It is owned by {3} and has been read {4} times.", 
+                              title, author, numberOfPages, owner, totalTimesRead);
+        }
+    }
+}
---
+++ PE/PE-Properties_Park/PE-Properties_Park/Program.cs
@@ -0,0 +1,101 @@
+﻿/*
+ * Hyunbin Park
+ * PE - Properties
+ * https://docs.google.com/document/d/1z9HVMUdQWjPEswC2UwoDFwaoHdpaLT1Yvo7KtKBBaYs/edit?usp=sharing
+ */
+
+namespace PE_Properties_Park
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Define variables
+            string title;       // Temporarily store the value of title
+            string author;      // Temporarily store the value of author
+            int numberOfPages;  // Temporarily store the value of number of pages
+            string owner;       // Temporarily store the value of owner
+            string userInput;   // Save the user's input
+
+
+            // Welcome
+            Console.WriteLine("Welcome to Book Simulator 2020");
+
+            // Prompt for values to creat a Book object
+            Console.Write("\nWhat is the book's title? ");
+            title = Console.ReadLine();
+            Console.Write("Who is the book's author? ");
+            author = Console.ReadLine();
+            Console.Write("How many pages does it have? ");
+            numberOfPages = int.Parse(Console.ReadLine());
+            Console.Write("Who is the book's current owner? ");
+            owner = Console.ReadLine();
+
+            // Creat a book object using user's inputs
+            Book b1 = new Book(title, author, numberOfPages, owner);
+
+
+            // Continually ask for input until they type "done"
+            do
+            {
+                Console.Write("\nWhat would you like to do?  ");
+                userInput = Console.ReadLine().ToLower().Trim();
+
+                // Determines what to do depending on user's input
+                switch(userInput)
+                {
+                    // Print the title of the book
+                    case "title":
+                        Console.WriteLine("The title is {0}.", b1.Title);
+                        break;
+
+                    // Print the author of the book
+                    case "author":
+                        Console.WriteLine("The author is {0}.", b1.Author);
+                        break;
+
+                    // Print the number of pages in the book
+                    case "pages":
+                        Console.WriteLine("The book has {0} pages.", b1.NumberOfPages);
+                        break;
+
+                    // Ask the user if they'd like to change the owner
+                    case "owner":
+                        Console.Write("Would you like to change the owner (yes/no)?  ");
+                        userInput = Console.ReadLine().ToLower().Trim();
+
+                        // If "yes", prompt for new owner and change the book's owner
+                        if (userInput == "yes")
+                        {
+                            Console.Write("Who is the new owner?  ");
+                            b1.Owner = Console.ReadLine();
+                            Console.WriteLine("The new owner is {0}.", b1.Owner);
+                        }
+                        // If "no", simply print the book's current owner
+                        else if (userInput == "no")
+                        {
+                            Console.WriteLine("Ok. {0} is still the owner.", b1.Owner);
+                        }
+                        break;
+
+                    // Add one to the total times read and print it
+                    case "read":
+                        b1.TotalTimesRead++;
+                        Console.WriteLine("The total times read is now {0}.", b1.TotalTimesRead);
+                        break;
+
+                    // Print all of the book's information
+                    case "print":
+                        b1.Print();
+                        break;
+
+                    // End the loop
+                    case "done":
+                        Console.WriteLine("Goodbye");
+                        break;
+                }
+            }
+            while(userInput != "done");
+        }
+    }
+}
